6a996bd129d8ff400d30b971a30cc99596f89ed4
Antonio Semeraro <antsemeraro@gmail.com>
2025-02-14 15:00:34 +0100

    Removed LockOnClose component for Camera conflict

--- App.tsx
+++ App.tsx
@@ -20,7 +20,6 @@ import JailMonkey from 'jail-monkey';
 import {WalletConnectProvider} from './src/contexts/WalletConnect';
 import {useWalletConnect} from './src/utils/walletConnect';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import LockOnClose from './src/components/LockOnClose';
 
 const App = () => {
   const isAuthorized = useSelector(makeSelectIsAuthorized);
@@ -107,7 +106,6 @@ const AppContainer = () => {
           <WalletConnectProvider>
             <PersistGate loading={null} persistor={persistor}>
               <App />
-              <LockOnClose />
             </PersistGate>
           </WalletConnectProvider>
         </PactProvider>
