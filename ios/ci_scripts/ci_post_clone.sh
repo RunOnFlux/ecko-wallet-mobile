ed969c2581d18d7180dcf03164f6b3105172e059
Antonio Semeraro <antsemeraro@gmail.com>
2025-04-20 09:45:07 +0200

    Adds CI post clone script
    Sets up the CI environment after cloning the repository

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+export NODE_OPTIONS=--max_old_space_size=8192
+
+brew install cocoapods
+brew install rbenv ruby-build
+brew install node@22
+brew link node@22
+brew install yarn
+
+echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
+echo ">>> INSTALL BUNDLER"
+ruby -v
+rbenv init
+rbenv install 2.7.8
+rbenv global 2.7.8
+eval "$(rbenv init -)"
+ruby -v
+gem install bundler --install-dir $GEM_HOME
+
+yarn
+bundle update --bundler
+yarn bundleinstall
+yarn podinstall
