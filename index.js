ee37740017563cc8119256f3d1573e211afb06dc
Antonio Semeraro <antsemeraro@gmail.com>
2025-06-09 19:29:00 +0200

    Preload kadena-crypto WASM on app startup
    Import the Emscripten binding in index.js so the WASM module initializes before any signature calls, preventing “.t of undefined” errors on first transaction

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import '@walletconnect/react-native-compat';
 import 'text-encoding-polyfill';
+import lib from 'cardano-crypto.js/kadena-crypto';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
